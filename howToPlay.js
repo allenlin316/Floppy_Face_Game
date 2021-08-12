28097d756c5bd25508b91147de71c5d45178cb36
allenlin316 <allenlin316@gmail.com>
2021-08-12 22:55:52 +0800

    modify howToPlay

--- howToPlay.js
+++ howToPlay.js
@@ -3,7 +3,7 @@ function HowToPlay() {
     this.show = () => {
         swal({
             title: "How To Play?",
-            text: "點擊螢幕維持鳥的飛行，撞到障礙物就輸了，通過一個障礙物加一分\n\n手機遊玩請點擊遊戲外的空白處，以避免放大 XD",
+            text: "點擊螢幕維持鳥的飛行，撞到障礙物就輸了，通過一個障礙物加一分",
             button: "OK",
         })
     }
